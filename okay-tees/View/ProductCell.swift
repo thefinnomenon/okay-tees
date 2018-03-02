be83aced69c68d6f06f44886c89dedaae7bf222d
Chris Finn <chris@finn.com>
2018-03-02 14:07:29 -0500

    + Product page view controller, collection view & cell + ProductCell view

---
+++ okay-tees/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  okay-tees
+//
+//  Created by Chris Finn on 3/2/18.
+//  Copyright © 2018 Finnovation LLC. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
